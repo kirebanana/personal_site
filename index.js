678fba244859adbacad92465a5755c30adfe8225
kirebanana <kirekaraivanov99@gmail.com>
2024-06-17 19:02:51 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+let flag1=0, flag2=0;
+function popupLeft(){
+    const popup = document.getElementById("leftPopup");
+    if(flag1==0){
+        popup.style.visibility = "hidden";
+        flag1 = 1;
+    }
+    else{
+        popup.style.visibility="visible"
+        flag1=0;
+    }
+}
+
+function popupRight(){
+    const popup = document.getElementById("rightPopup");
+    if(flag2==0){
+        popup.style.visibility = "hidden";
+        flag1 = 1;
+    }
+    else{
+        popup.style.visibility="visible"
+        flag1=0;
+    }
+}
